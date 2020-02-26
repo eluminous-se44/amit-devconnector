eed2f6f7e4fc376fad504ceb3b7f17ea712f428a
Amit <eluminous_se42@eluminoustechnologies.com>
2020-02-26 14:29:48 +0530

    Initial commit of devconnector api created of users,posts,profile

---
+++ models/User.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const UserSchema = mongoose.Schema({
+
+	name: {
+		type: String,
+		required: true
+	},
+
+	email: {
+		type: String,
+		required: true
+	},
+
+	password: {
+
+		type:String,
+		required:true
+	},
+
+	date: {
+
+		type: Date,
+		default: Date.now
+	}
+
+})
+
+module.exports = User = mongoose.Model('users',UserSchema);
+
+
